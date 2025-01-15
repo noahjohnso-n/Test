dfca0092de9c65eb3e5be9503f01af3bd613fbee
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-15 14:42:12 -0500

    test 5

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import env from "dotenv";
 import pg from "pg";
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
 env.config();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = new pg.Client({
@@ -19,6 +20,18 @@ const db = new pg.Client({
   
 db.connect();
 
+db.query("select * from users", (err, res) => {
+    if(err){
+        console.log("ERROR: " + err.stack);
+    }else{
+        const r = res.rows;
+        console.log("OUTPUT: " + r.length);
+        for(var i = 0 ; i < r.length ; i++){
+            console.log(r[i].email);
+        }
+    }
+});
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
@@ -27,4 +40,4 @@ app.listen(port, (req, res) => {
     console.log(`Server now listening on port ${port}`);
 })
 
-db.end();
+// db.end;
