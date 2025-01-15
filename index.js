5f48a036cbf662e38711f3afdba0e15fdfa250ee
Noah Johnson <noahjohnson@Noahs-MacBook-Air.local>
2025-01-15 10:36:24 -0500

    Database test 1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import env from "dotenv";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+env.config();
+
 app.use(express.static("public"));
+
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    ssl: { rejectUnauthorized: false }
+});
+  
+db.connect();
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
